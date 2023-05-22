e92d984774794417344f1aa3ebf7855444a7253e
Jav Nitram <1954675+javnitram@users.noreply.github.com>
2023-05-22 18:01:07 +0200

    Documenta y limpia código. Mejora solicitar_hosts

--- gestion_aula.sh
+++ gestion_aula.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 source .config
 
+# Provoca que el script termine si se usa una variable no declarada
 set -u
 
-# Variables globales
-declare -a HOSTS=()
+# Variables globales...
+# ... para configurar algunos parámetros de comandos parallel-*
 declare -a SHORT_OPTS=()
 declare -a LONG_OPTS=()
-RUTA_LOCAL=""
-RUTA_REMOTA=""
-USUARIO_REMOTO=""
-PASS_USUARIO_REMOTO=""
 SHORT_OPTS=("-l" "root" "-t" "$TIMEOUT" "${COMMON_OPTS[@]}")
 TMP_DIR=$(mktemp -d)
 TMP_STDOUT_DIR="${TMP_DIR}/stdout/"
 TMP_STDERR_DIR="${TMP_DIR}/stderr/"
 LONG_OPTS=("${SHORT_OPTS[@]}" "-o" "$TMP_STDOUT_DIR/" "-e" "$TMP_STDERR_DIR/")
+
+# ... para solicitar (y recordar como predefinidos) valores indicados por el usuario
+declare -a HOSTS=()
+RUTA_LOCAL=""
+RUTA_REMOTA=""
+USUARIO_REMOTO=""
+PASS_USUARIO_REMOTO=""
 
 function dialogo() {
     local mensaje
@@ -23,7 +27,7 @@ function dialogo() {
     mensaje="$1"
     aceptar=${2:-Aceptar}
 
-    # Si se quiere marcar por defecto Cancelar, añadir -s /^C
+    # Si se quiere marcar por defecto Cancelar, usar -s /Cancelar
     smenu -d -2 "$aceptar" -1 ^C -s /"$aceptar" -x q 30 \
         -m "$mensaje"   \
         <<< "$aceptar Cancelar"
@@ -42,6 +46,10 @@ function confirmar_comando() {
         return_code=0
     else
         # Ejecutar comando y mostrarlo tal cual lo lanza bash
+        # Para ello ponemos temporalmente el modo debug con set -x
+        # "$@" se expande a una lista de parámetros entrecomillados,
+        # es decir, "$1" "$2" ... cada uno se trata de forma 
+        # independiente y se evitan problemas con espacios en blanco 
         set -x
         "$@"
         { return_code=$?; } 2>/dev/null
@@ -69,25 +77,33 @@ function solicitar_ruta_remota() {
 function solicitar_cadena() {
     local mensaje
     local valor_defecto
-    local ruta
+    local cadena
     mensaje="$1"
     valor_defecto="$2"
 
-    # -e Permite autocompletar con tabulador, -i inserta un valor por defecto
-    read -e -r -p "$mensaje" -i "$valor_defecto" ruta
-    ruta=${ruta//\\/}
-    #ruta=$(printf %q "$ruta")
+    # -e Permite autocompletar con tabulador, -i inserta el $valor_defecto recibido
+    read -e -r -p "$mensaje" -i "$valor_defecto" cadena
+    # Autocompletar puede escapar espacios con '\ ', pero los quitamos
+    # porque la cadena se utilizará siempre entre comillas dobles, que también 
+    # evitan problemas al interpretar los espacios en blanco.
+    cadena=${cadena//\\/}
 
-    echo "$ruta"
+    # Si se prefiere escapar caracteres especiales, podemos usar la siguiente
+    # instrucción
+    #cadena=$(printf %q "$cadena")
+
+    echo "$cadena"
 }
 
 function solicitar_hosts() {
     local start
     local hosts
 
-    start=$(head -n 1 "$HOSTS_FILE" | sed 's/.*[^0-9]\(\d*\)/\1/')
+    # Identifica el número más bajo de la lista de hosts
+    # para calcular los números de opción que propondrá smenu
+    start=$(sort "$HOSTS_FILE" | head -n 1 | sed 's/.*[^0-9]\(\d*\)/\1/')
     ((start--))
-    hosts=$(sed -e '$aTodos' "$HOSTS_FILE" \
+    hosts=$( (sort "$HOSTS_FILE" && echo 'Todos') \
             | smenu -m 'Hosts (puedes marcar varios usando "t"): ' -d -s '#last' -a t:4,b c:4,bu ct:0/4,bu -T" " -p -N -D s:$start )
     if  [[ -z "$hosts" ]]; then
         exit 0
@@ -105,7 +121,6 @@ function accion_09_ver_espacio_HD() {
     comando=("parallel-ssh" "-i" "${SHORT_OPTS[@]}" "${HOSTS[@]}" "df -h | egrep '/home$'")
     confirmar_comando "${comando[@]}"
 }
-
 
 function accion_10_ver_espacio_SSD() {
     solicitar_hosts
@@ -176,15 +191,25 @@ function main() {
             [[ -z $accion ]] && exit 0
             echo "Acción elegida: $accion"
 
+            # Rellenar 0 por la izquierda, si necesario
             [[ "$accion" == [1-9]_* ]] && accion="0$accion"
+            # Identificar función que implementa la acción a ejecutar
             funcion="accion_$accion"
 
             if "$funcion"; then
-                :; # Comando ejecutado sin errores o confirmación cancelada
+                :; # Función ejecutada sin errores o cancelada
             elif [[ -d "$TMP_STDERR_DIR" ]]; then
+                # Esta ruta existe si ha habido errores y el comando se lanzó con opción -e <dir>
+                # para redirigir errores a un fichero por cada conexión
                 confirmacion=$(dialogo "Hubo algunos errores, ¿mostrar en detalle?" "Mostrar")
                 if [[ ! "$confirmacion" == "Cancelar" ]]; then 
                     echo "Detalle de errores:"
+                    # Mostramos errores, para ello usamos grep que, cuando recibe
+                    # varios ficheros, identifica en cada línea el nombre del fichero
+                    # que la contiene
+                    # A continuación dejamos únicamente el nombre del fichero (sin el
+                    # directorio padre), dicho nombre identifica la conexión
+                    # que produjo fallos
                     grep ".*" "$TMP_STDERR_DIR"/* 2> /dev/null | sed 's/.*stderr\/*//g'
                 fi
             fi
@@ -201,6 +226,3 @@ if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
     # Están ejecutando directamente este script, no importándolo con source
     main "$@"
 fi
-
-#  Filtrar fallidos
-#  grep "FAILURE" prueba.log | sed 's/.* \[[A-Za-z]*\] \(\S*\).*/\1/' | tr '\n' ' '
