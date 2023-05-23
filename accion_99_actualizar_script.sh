4c4efebb6ad98b8b34aabf5c77f500cb42faba0e
Jav Nitram <1954675+javnitram@users.noreply.github.com>
2023-05-23 13:37:21 +0200

    Fix accion_99_actualizar_script

--- accion_99_actualizar_script.sh
+++ accion_99_actualizar_script.sh
@@ -2,12 +2,7 @@
 function accion_99_actualizar_script() {
     local script_dir
     script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-    pushd "$script_dir" || exit 1
-    if git diff --exit-code main origin/main > /dev/null; then
-        popd || exit 1
-    else
-        git pull
-        exit 0
-    fi
-    
+    cd "$script_dir" || exit 1
+    git pull
+    exit 0   
 }
