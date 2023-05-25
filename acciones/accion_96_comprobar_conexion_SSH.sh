35144b45265015304d1f4671f0a954d2db8570ed
Jav Nitram <1954675+javnitram@users.noreply.github.com>
2023-05-25 10:17:40 +0200

    Añade accion_96_comprobar_conexión_SSH

---
+++ acciones/accion_96_comprobar_conexion_SSH.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###############################################################################
+# Script(s) de gestión de aula
+# @author https://github.com/javnitram/
+# GNU GENERAL PUBLIC LICENSE Version 3
+# Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+###############################################################################
+
+function accion_96_comprobar_conexión_SSH() {
+    solicitar_hosts
+    comando_remoto='echo "$(whoami)@(hostname): $(uptime -p)"'
+    comando=("parallel-ssh" -i "${SHORT_OPTS[@]}" "${HOSTS[@]}" "$comando_remoto")
+    confirmar_comando "${comando[@]}"
+}
