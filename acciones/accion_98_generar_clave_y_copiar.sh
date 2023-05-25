d8b56c11ceed9a5e0658c982e446aff9d67c83ec
Jav Nitram <1954675+javnitram@users.noreply.github.com>
2023-05-25 10:17:00 +0200

    Confirmación en accion_98_generar_clave_y_copiar

--- acciones/accion_98_generar_clave_y_copiar.sh
+++ acciones/accion_98_generar_clave_y_copiar.sh
@@ -7,41 +7,45 @@
 ###############################################################################
 
 function accion_98_generar_clave_y_copiar() {
-    solicitar_hosts
-    local linea
-    local usuario
-    local host
-    local puerto
-    local comando
-    local password
-    local ok
+    params=("$(describe_accion "${FUNCNAME[0]}")" "Esta acción debe realizarse una única vez por cada host para que no pida contraseña por SSH para el usuario configurado. ¿Continuar?" "Aceptar") 
+    if dialogo "${params[@]}"; then 
+        solicitar_hosts
+        local linea
+        local usuario
+        local host
+        local puerto
+        local comando_lista_hosts
+        local password
+        declare -a comando=("return_code=0;")
 
-    ok=true
-    if [[ ${HOSTS[0]} == "-h" ]]; then
-        comando="leer_fichero_hosts ${HOSTS[1]}"
-    else
-        HOSTS[0]=""
-        comando="echo ${HOSTS[*]}"
-    fi
-
-    ssh-keygen -t rsa
-    which sshpass >& /dev/null && password=$(dialogo_password_oculto)
-    for linea in $($comando)
-    do
-        usuario=""
-        host=""
-        puerto=""
-        [[ $linea =~ "@" ]] && usuario=${linea/@*/}
-        usuario=${usuario:-root}
-        host=${linea/*@/}
-        host=${host/:*/}
-        [[ $linea =~ ":" ]] && puerto=${linea##*:}
-        puerto=${puerto:-22}
-        if which sshpass >& /dev/null; then
-            sshpass -p "$password" ssh-copy-id -i ~/.ssh/id_rsa.pub -p "$puerto" "$usuario@$host" || ok=false
+        if [[ ${HOSTS[0]} == "-h" ]]; then
+            comando_lista_hosts="leer_fichero_hosts ${HOSTS[1]}"
         else
-            ssh-copy-id -i ~/.ssh/id_rsa.pub -p "$puerto" "$usuario@$host" || ok=false
-        fi            
-    done
-    $ok
+            HOSTS[0]=""
+            comando_lista_hosts="echo ${HOSTS[*]}"
+        fi
+
+        ssh-keygen -t rsa
+        which sshpass >& /dev/null && password=$(dialogo_password_oculto)
+        for linea in $($comando_lista_hosts)
+        do
+            usuario=""
+            host=""
+            puerto=""
+            [[ $linea =~ "@" ]] && usuario=${linea/@*/}
+            usuario=${usuario:-root}
+            host=${linea/*@/}
+            host=${host/:*/}
+            [[ $linea =~ ":" ]] && puerto=${linea##*:}
+            puerto=${puerto:-22}
+            if which sshpass >& /dev/null; then
+                comando+=("sshpass -p $password ssh-copy-id -i ~/.ssh/id_rsa.pub -p $puerto $usuario@$host || return_code=1;")
+            else
+                comando+=("ssh-copy-id -i ~/.ssh/id_rsa.pub -p $puerto $usuario@$host || return_code=1;")
+            fi            
+        done
+        comando+=('exit $return_code')
+        comando=("bash" "-c" "${comando[*]}")
+        confirmar_comando "${comando[@]}"
+    fi
 }
