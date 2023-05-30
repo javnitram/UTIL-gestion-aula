d3afc465bce1d24931ae3b1c332ac88f119a4b78
Jav Nitram <1954675+javnitram@users.noreply.github.com>
2023-05-30 16:57:38 +0200

    Acciones para habilitar y deshabilitar modo examen

--- acciones/accion_52_deshabilitar_modo_examen.sh
+++ acciones/accion_52_deshabilitar_modo_examen.sh
@@ -6,21 +6,19 @@
 # Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 ###############################################################################
 
-# Variables globales a compartir con accion_52_deshabilitar_modo_examen
-# Valores por defecto, se solicitarán al usuario de forma interactiva
-USUARIO_EXAMEN="examen1"
-USUARIO_ALUMNO="alumnotd"
+# Variables globales compartidas con accion_51_habilitar_modo_examen
+# USUARIO_EXAMEN y USUARIO_ALUMNO
 
-function accion_51_habilitar_modo_examen() {
+function accion_52_deshabilitar_modo_examen() {
     local f
     local acciones
     declare -a opciones
 
     solicitar_hosts
 
-    if dialogo "$(describe_accion "${FUNCNAME[0]}")" "Paso 1 - Se sugiere impedir el uso del usuario alumnotd/alumnotv"
+    if dialogo "$(describe_accion "${FUNCNAME[0]}")" "Paso 1 - Volver a permitir el uso del usuario alumnotd/alumnotv"
     then 
-        opciones=(accion_23_bloquear_usuario ON accion_22_finalizar_procesos_y_sesión_de_usuario ON)
+        opciones=(accion_24_desbloquear_usuario ON)
 
         local nOpciones
         nOpciones=$(altura_opciones_menu "${opciones[@]}")
@@ -43,9 +41,9 @@ function accion_51_habilitar_modo_examen() {
         fi
     fi
 
-    if dialogo "$(describe_accion "${FUNCNAME[0]}")" "Paso 2 - Se sugiere forzar el uso del usuario examen1/examen2 con permisos controlados:"
+    if dialogo "$(describe_accion "${FUNCNAME[0]}")" "Paso 2 - Se sugiere impedir el uso del usuario de examen"
     then
-        opciones=(accion_18_copiar ON accion_03_cambiar_contraseña ON accion_20_dar_permisos ON)
+        opciones=(accion_22_finalizar_procesos_y_sesión_de_usuario ON accion_03_cambiar_contraseña ON accion_23_bloquear_usuario ON)
 
         local nOpciones
         nOpciones=$(altura_opciones_menu "${opciones[@]}")
@@ -60,17 +58,10 @@ function accion_51_habilitar_modo_examen() {
             for f in $acciones
             do
                 f="${f//\"/}"
-                if [[ "$f" == "accion_18_copiar" ]]; then
-                    RUTA_LOCAL=""
-                    solicitar_ruta_local || return 1
-                    RUTA_REMOTA="/home/${USUARIO_REMOTO}/Escritorio/"
-                    solicitar_ruta_remota || return 1
-                fi
                 if dialogo "$(describe_accion "${FUNCNAME[0]}")" "Siguiente acción '$(describe_accion "$f")', ¿continuar?" "Continuar"; then
                     ejecutar "$f"
                 fi
             done
         fi
     fi
-    dialogo "$(describe_accion "${FUNCNAME[0]}")" "Al finalizar el examen recuerda:\n (1) desbloquear el usuario '$USUARIO_ALUMNO' y \n (2) cambiar la contraseña del usuario '$USUARIO_EXAMEN'"
 }
