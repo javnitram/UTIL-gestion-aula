e973e7ae28c3a9249ddb795a8731198b84872866
Jav Nitram <1954675+javnitram@users.noreply.github.com>
2023-06-06 17:14:28 +0200

    Mostrar usuarios bloqueados

--- acciones/accion_01_comprobar_conexiones_y_sesiones.sh
+++ acciones/accion_01_comprobar_conexiones_y_sesiones.sh
@@ -8,7 +8,10 @@
 
 function accion_01_comprobar_conexiones_y_sesiones() {
     solicitar_hosts \
-    && comando_remoto='echo "$(whoami)@$(hostname). Tiempo encendido: $(uptime -p). Sesiones abiertas: $(users)"' \
+    && comando_remoto='echo "$(whoami)@$(hostname): 
+    Tiempo encendido: $(uptime -p)
+    Sesiones abiertas: $(users)
+    Usuarios bloqueados: $(re=$(grep -v "nologin$" /etc/passwd | awk -F: '\''{ if ($1 != "sync") { print $1 } }'\'' | tr "\n" "|") && re=${re%|} && passwd -aS | awk -v re="$re" '\''{ if (($1 ~ "^"re"$") && ($2=="L")) { print $1 } }'\'' | tr "\n" " ")"' \
     && comando=("parallel-ssh" -i "${SHORT_OPTS[@]}" "${HOSTS[@]}" "$comando_remoto") \
     && confirmar_comando "${comando[@]}"
 }
